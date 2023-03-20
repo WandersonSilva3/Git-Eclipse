229636be50b56e191dfd6cf8d796c4170734c1a0
wadnersonsilva3 <wandersonswps1409@gmail.com>
2023-03-20 09:17:42 -0300

    test repository

---
+++ src/operadores/Aula2.java
@@ -0,0 +1,12 @@
+package operadores;
+
+public class Aula2 {
+
+	public static void main(String[] args) {
+
+		String nomeCompleto = 1+ "1" + 1 + " 2- Linguagem " + "Java";
+		
+		System.out.println(nomeCompleto);
+	}
+
+}
---
+++ src/operadores/Aula3.java
@@ -0,0 +1,18 @@
+package operadores;
+
+public class Aula3 {
+
+	public static void main(String[] args) {
+
+		int numero = 5;
+		
+		System.out.println(numero);
+		
+		numero = - numero;
+		System.out.println(numero);
+		
+		numero = -1 * numero;
+		System.out.println(numero);
+	}
+
+}
