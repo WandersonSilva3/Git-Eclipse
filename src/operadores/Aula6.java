b2769e7a5a9d84ad401061c0ab94badd28d8b94b
wandersonsilva1 <wandersonwps81@gmail.com>
2023-03-20 10:20:02 -0300

    operadoes relacionais

---
+++ src/operadores/Aula6.java
@@ -0,0 +1,17 @@
+package operadores;
+
+public class Aula6 {
+
+	public static void main(String[] args) {
+
+		int num1 = 1;
+		int num2 = 2;
+		
+		boolean simNao = num1 == num2;
+		System.out.println(simNao);
+		
+		simNao = num1 < num2;
+		System.out.println(simNao);
+	}
+
+}
