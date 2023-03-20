c2ec453d56bc111b70af4f4185674dea02ba2dbd
wandersonsilva1 <wandersonwps81@gmail.com>
2023-03-20 10:06:35 -0300

    operador ternário

---
+++ src/operadores/Aula5.java
@@ -0,0 +1,16 @@
+package operadores;
+
+public class Aula5 {
+
+	public static void main(String[] args) {
+
+		int a, b;
+		
+		a = 5;
+		b = 6;
+		
+		String resultado = a!=b ? "verdadeiro" : "falso";
+		System.out.println(resultado);
+	}
+
+}
