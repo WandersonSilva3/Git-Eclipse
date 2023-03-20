bec2ccfbe2f8c7a05784320d708740aeed562942
wandersonsilva1 <wandersonwps81@gmail.com>
2023-03-20 09:49:53 -0300

    adicionando boolean

---
+++ src/operadores/Aula4.java
@@ -0,0 +1,21 @@
+package operadores;
+
+public class Aula4 {
+
+	public static void main(String[] args) {
+
+		int num = 5;
+		
+		System.out.println(--num);
+		
+		System.out.println(++num);
+		
+		System.out.println(++num);
+		
+		boolean ligado = true;
+		System.out.println(ligado);
+		
+		ligado = !ligado;
+		System.out.println(ligado);
+	}
+}
